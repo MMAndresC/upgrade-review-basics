4a52f7e90ed5614af8825a11d4f4dc3afaa2e489
MMAndresC <mandrescasabon@hotmail.com>
2022-05-17 23:36:37 +0200

    Iteracion#4

--- reviewbasics.js
+++ reviewbasics.js
@@ -74,3 +74,17 @@ for (let user of users){
     }
 }
 console.log(sounds);
+
+//Iteracion#4
+//Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando 
+//el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+const array = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+
+function findArrayIndex(array,text){
+    return (array.indexOf(text));
+}
+console.log(findArrayIndex(array,'diplodocus'));
+console.log(findArrayIndex(array, 'Mosquito'));
+console.log(findArrayIndex(array, 'Caracol'));
+console.log(findArrayIndex(array, 'caracol'));
+console.log(findArrayIndex(array, 'Ajolote'));
