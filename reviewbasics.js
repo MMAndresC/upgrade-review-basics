25b380981aa18a3a79b48aae17e848a617ad7b75
MMAndresC <mandrescasabon@hotmail.com>
2022-05-17 23:29:16 +0200

    Iteracion#3

--- reviewbasics.js
+++ reviewbasics.js
@@ -62,3 +62,15 @@ for (let user of users){
     }
 }
 console.log(total/cont);
+
+//Iteracion#3
+//Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. 
+//Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+//Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+const sounds = [];
+for (let user of users){
+    for(let key in user.favoritesSounds){
+        sounds[key] ? sounds[key]++ : sounds[key] = 1;
+    }
+}
+console.log(sounds);
